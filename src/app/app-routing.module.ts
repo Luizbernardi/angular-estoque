b3cc2fd33e65ed30ab8db842b2372a915fe54856
luiz bernardi <luizbernardi37@gmail.com>
2024-11-04 15:21:47 -0300

    fix arrumadno links navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProdutoListComponent } from './produto-list/produto-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'produtos-list' , component: ProdutoListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
